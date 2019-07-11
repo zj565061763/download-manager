8a610ba77f6fb88070e9c66eb5c701c395763f33
zjun <565061763@qq.com>
2019-07-11 09:57:15 +0800

    DownloadState.isActive()

--- lib/src/main/java/com/sd/lib/dldmgr/DownloadState.java
+++ lib/src/main/java/com/sd/lib/dldmgr/DownloadState.java
@@ -5,5 +5,15 @@ public enum DownloadState
     Prepare,
     Downloading,
     Success,
-    Error,
+    Error;
+
+    /**
+     * 是否处于活动状态，{@link #Prepare}或者{@link #Downloading}
+     *
+     * @return
+     */
+    public boolean isActive()
+    {
+        return this == Prepare || this == Downloading;
+    }
 }
