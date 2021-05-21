918a7a9f817f973c15ab73fa3df0ad8f54759c76
zjun <565061763@qq.com>
2021-05-21 21:59:07 +0800

    kotlin DownloadState

--- lib/src/main/java/com/sd/lib/dldmgr/DownloadState.kt
+++ lib/src/main/java/com/sd/lib/dldmgr/DownloadState.kt
@@ -1,19 +1,19 @@
-package com.sd.lib.dldmgr;
+package com.sd.lib.dldmgr
 
-public enum DownloadState
-{
+enum class DownloadState {
+    /** 准备下载 */
     Prepare,
+
+    /** 下载中 */
     Downloading,
+
+    /** 下载成功 */
     Success,
+
+    /** 下载失败 */
     Error;
 
-    /**
-     * 是否处于完成状态，{@link #Success}或者{@link #Error}
-     *
-     * @return
-     */
-    public boolean isCompleted()
-    {
-        return this == Success || this == Error;
-    }
+    /** 是否处于完成状态，[Success]或者[Error] */
+    val isCompleted: Boolean
+        get() = this == Success || this == Error
 }
