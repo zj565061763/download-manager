54b23626cc6003e40299efaee00fecc04e9ce114
zjun <565061763@qq.com>
2021-05-21 21:56:15 +0800

    kotlin DownloadError

--- lib/src/main/java/com/sd/lib/dldmgr/DownloadError.kt
+++ lib/src/main/java/com/sd/lib/dldmgr/DownloadError.kt
@@ -1,37 +1,27 @@
-package com.sd.lib.dldmgr;
+package com.sd.lib.dldmgr
 
-public enum DownloadError
-{
-    /**
-     * 获取临时文件失败
-     */
+enum class DownloadError {
+    /** 创建临时文件失败 */
     CreateTempFile,
-    /**
-     * 提交下载任务失败
-     */
+
+    /** 提交下载任务失败 */
     SubmitFailed,
-    /**
-     * 临时文件文件不存在
-     */
+
+    /** 临时文件不存在 */
     TempFileNotExists,
-    /**
-     * 获取下载文件失败
-     */
+
+    /** 创建下载文件失败 */
     CreateDownloadFile,
-    /**
-     * 临时文件重命名为下载文件失败
-     */
+
+    /** 临时文件重命名为下载文件失败 */
     RenameFile,
-    /**
-     * Http请求异常
-     */
+
+    /** Http请求异常 */
     Http,
-    /**
-     * 下载被取消
-     */
+
+    /** 下载被取消 */
     Cancel,
-    /**
-     * 其他未知异常
-     */
+
+    /** 其他未知异常 */
     Other
 }
